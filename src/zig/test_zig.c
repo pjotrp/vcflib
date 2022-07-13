ee3df27d1f3e11262a75e15145c890ea6acf8fcc
Pjotr Prins <pjotr.public01@thebird.nl>
2022-07-13 11:59:05 +0200

    Pass string in

--- src/zig/test_zig.c
+++ src/zig/test_zig.c
@@ -1,9 +1,9 @@
 
 #include <stdio.h>
 
-extern char *hello_zig();
+extern char *hello_zig(char *msg);
 
 int main(int argc, char **argv) {
-    printf("%s\n",hello_zig("hello world"));
+    printf("%s\n",hello_zig("hello world from C!"));
     return 0;
 }
